cf359973a4403e5e02750167a06703863d15680b
Jonathan Yee <webber989@gmail.com>
2018-07-28 16:13:26 -0600

    Working on the release search gui

--- src/jmusicreleasemanager/guiframe.java
+++ src/jmusicreleasemanager/guiframe.java
@@ -16,10 +16,14 @@ import org.jdatepicker.JDatePicker;
 import javax.swing.JButton;
 import javax.swing.JToggleButton;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
 public class guiframe extends JFrame{
 	private JTextField artistfield;
@@ -27,8 +31,12 @@ public class guiframe extends JFrame{
 	private JTextField urlfield;
 	private JComboBox<?> comboBoxReleaseType;
 	private sqlfunctions mydatabase;
-	private JDatePicker datepicker;
+	private JDatePicker newReleaseDate;
+	private JDatePicker startReleaseDate;
+	private JDatePicker endReleaseDate;
 	private JTextField datefield;
+	private JTextPane textPane;
+	private JTextArea textAreaDisplayReleases;
 	
 	public guiframe(sqlfunctions mydatabase) {
 		setResizable(false);
@@ -53,58 +61,109 @@ public class guiframe extends JFrame{
 		gbc_tabbedPane.gridy = 0;
 		getContentPane().add(tabbedPane, gbc_tabbedPane);
 		
-		JPanel searchdbpanel = new JPanel();
-		tabbedPane.addTab("Search the database", null, searchdbpanel, null);
-		GridBagLayout gbl_searchdbpanel = new GridBagLayout();
-		gbl_searchdbpanel.columnWidths = new int[]{0};
-		gbl_searchdbpanel.rowHeights = new int[]{0};
-		gbl_searchdbpanel.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_searchdbpanel.rowWeights = new double[]{Double.MIN_VALUE};
-		searchdbpanel.setLayout(gbl_searchdbpanel);
+		JPanel searchReleases = new JPanel();
+		tabbedPane.addTab("Search the database", null, searchReleases, null);
+		GridBagLayout gbl_searchReleases = new GridBagLayout();
+		gbl_searchReleases.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_searchReleases.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_searchReleases.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_searchReleases.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		searchReleases.setLayout(gbl_searchReleases);
 		
-		JPanel addtodbpanel = new JPanel();
-		tabbedPane.addTab("Add to the database", null, addtodbpanel, null);
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				java.util.GregorianCalendar Date = (GregorianCalendar) startReleaseDate.getModel().getValue();
+				Calendar calendar = (Calendar)Date;
+				java.sql.Date afterdate = new java.sql.Date(calendar.getTimeInMillis());
+				java.sql.Date beforedate = null; 
+				if(endReleaseDate.getModel().getValue()!=null) {
+					Date = (GregorianCalendar) endReleaseDate.getModel().getValue();
+					calendar = (Calendar)Date;
+					beforedate = new java.sql.Date(calendar.getTimeInMillis());
+				}
+				ResultSet rs = mydatabase.retrieveReleases(mydatabase, afterdate, beforedate);
+				try {
+					while(rs.next()) {
+						textAreaDisplayReleases.append(rs.getString("name")+" "+rs.getString("artist")+" "+rs.getDate("date").toString()+" "+rs.getString("type")+"\n");
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		GridBagConstraints gbc_btnSubmit = new GridBagConstraints();
+		gbc_btnSubmit.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSubmit.gridx = 0;
+		gbc_btnSubmit.gridy = 0;
+		searchReleases.add(btnSubmit, gbc_btnSubmit);
+		
+		JButton btnClearAll_1 = new JButton("Clear All");
+		btnClearAll_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textAreaDisplayReleases.setText("");
+				startReleaseDate.getFormattedTextField().setText("");
+				endReleaseDate.getFormattedTextField().setText("");
+			}
+		});
+		GridBagConstraints gbc_btnClearAll_1 = new GridBagConstraints();
+		gbc_btnClearAll_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnClearAll_1.gridx = 1;
+		gbc_btnClearAll_1.gridy = 0;
+		searchReleases.add(btnClearAll_1, gbc_btnClearAll_1);
+		
+		JLabel lblDatesAfter = new JLabel("Dates After");
+		GridBagConstraints gbc_lblDatesAfter = new GridBagConstraints();
+		gbc_lblDatesAfter.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDatesAfter.gridx = 0;
+		gbc_lblDatesAfter.gridy = 1;
+		searchReleases.add(lblDatesAfter, gbc_lblDatesAfter);
+		
+		JLabel lblDatesBefore = new JLabel("Dates Before");
+		GridBagConstraints gbc_lblDatesBefore = new GridBagConstraints();
+		gbc_lblDatesBefore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDatesBefore.gridx = 1;
+		gbc_lblDatesBefore.gridy = 1;
+		searchReleases.add(lblDatesBefore, gbc_lblDatesBefore);
+		
+		startReleaseDate = new JDatePicker();
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 2;
+		searchReleases.add(startReleaseDate, gbc_lblNewLabel);
+		
+		endReleaseDate = new JDatePicker();
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 2;
+		searchReleases.add(endReleaseDate, gbc_lblNewLabel_1);
+		
+		textAreaDisplayReleases = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.gridwidth = 3;
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 3;
+		searchReleases.add(textAreaDisplayReleases, gbc_textArea);
+		
+		JPanel addNewRelease = new JPanel();
+		tabbedPane.addTab("Add to the database", null, addNewRelease, null);
 		GridBagLayout gbl_addtodbpanel = new GridBagLayout();
 		gbl_addtodbpanel.columnWidths = new int[]{0, 0, 0, 0};
 		gbl_addtodbpanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_addtodbpanel.columnWeights = new double[]{0.0, 1.0, 1.0, Double.MIN_VALUE};
 		gbl_addtodbpanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		addtodbpanel.setLayout(gbl_addtodbpanel);
+		addNewRelease.setLayout(gbl_addtodbpanel);
 		
-		JButton btnClearAll = new JButton("Clear All");
-		btnClearAll.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				artistfield.setText("");
-				releasenamefield.setText("");
-				comboBoxReleaseType.setSelectedIndex(-1);
-				urlfield.setText("");
-				//reset the date box??
-			}
-		});
-		GridBagConstraints gbc_btnClearAll = new GridBagConstraints();
-		gbc_btnClearAll.insets = new Insets(0, 0, 5, 5);
-		gbc_btnClearAll.gridx = 0;
-		gbc_btnClearAll.gridy = 0;
-		addtodbpanel.add(btnClearAll, gbc_btnClearAll);
-		
-		JToggleButton tglbtnSubmit = new JToggleButton("Submit");
-		tglbtnSubmit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-									
-			}
-		});
-		GridBagConstraints gbc_tglbtnSubmit = new GridBagConstraints();
-		gbc_tglbtnSubmit.insets = new Insets(0, 0, 5, 5);
-		gbc_tglbtnSubmit.gridx = 1;
-		gbc_tglbtnSubmit.gridy = 0;
-		addtodbpanel.add(tglbtnSubmit, gbc_tglbtnSubmit);
 		
 		JLabel lblartist = new JLabel("Artist");
 		GridBagConstraints gbc_lblartist = new GridBagConstraints();
 		gbc_lblartist.insets = new Insets(0, 0, 5, 5);
 		gbc_lblartist.gridx = 0;
 		gbc_lblartist.gridy = 1;
-		addtodbpanel.add(lblartist, gbc_lblartist);
+		addNewRelease.add(lblartist, gbc_lblartist);
 		
 		artistfield = new JTextField();
 		GridBagConstraints gbc_releasenamefield = new GridBagConstraints();
@@ -113,7 +172,7 @@ public class guiframe extends JFrame{
 		gbc_releasenamefield.fill = GridBagConstraints.HORIZONTAL;
 		gbc_releasenamefield.gridx = 1;
 		gbc_releasenamefield.gridy = 1;
-		addtodbpanel.add(artistfield, gbc_releasenamefield);
+		addNewRelease.add(artistfield, gbc_releasenamefield);
 		artistfield.setColumns(10);
 		
 		JLabel lblReleaseName = new JLabel("Release Name");
@@ -121,7 +180,7 @@ public class guiframe extends JFrame{
 		gbc_lblReleaseName.insets = new Insets(0, 0, 5, 5);
 		gbc_lblReleaseName.gridx = 0;
 		gbc_lblReleaseName.gridy = 2;
-		addtodbpanel.add(lblReleaseName, gbc_lblReleaseName);
+		addNewRelease.add(lblReleaseName, gbc_lblReleaseName);
 		
 		releasenamefield = new JTextField();
 		GridBagConstraints gbc_releasenamefield1 = new GridBagConstraints();
@@ -130,7 +189,7 @@ public class guiframe extends JFrame{
 		gbc_releasenamefield1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_releasenamefield1.gridx = 1;
 		gbc_releasenamefield1.gridy = 2;
-		addtodbpanel.add(releasenamefield, gbc_releasenamefield1);
+		addNewRelease.add(releasenamefield, gbc_releasenamefield1);
 		releasenamefield.setColumns(10);
 		
 		JLabel lblreleasetype = new JLabel("Release Type");
@@ -138,7 +197,7 @@ public class guiframe extends JFrame{
 		gbc_lblreleasetype.insets = new Insets(0, 0, 5, 5);
 		gbc_lblreleasetype.gridx = 0;
 		gbc_lblreleasetype.gridy = 3;
-		addtodbpanel.add(lblreleasetype, gbc_lblreleasetype);
+		addNewRelease.add(lblreleasetype, gbc_lblreleasetype);
 		
 		comboBoxReleaseType = new JComboBox();
 		comboBoxReleaseType.setModel(new DefaultComboBoxModel(new String[] {"album", "single", "ep", "compilation", "minialbum"}));
@@ -148,30 +207,29 @@ public class guiframe extends JFrame{
 		gbc_comboBoxReleaseType.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBoxReleaseType.gridx = 1;
 		gbc_comboBoxReleaseType.gridy = 3;
-		addtodbpanel.add(comboBoxReleaseType, gbc_comboBoxReleaseType);
+		addNewRelease.add(comboBoxReleaseType, gbc_comboBoxReleaseType);
 		
 		JLabel lblReleaseDate = new JLabel("Release Date");
 		GridBagConstraints gbc_lblReleaseDate = new GridBagConstraints();
 		gbc_lblReleaseDate.insets = new Insets(0, 0, 5, 5);
 		gbc_lblReleaseDate.gridx = 0;
 		gbc_lblReleaseDate.gridy = 4;
-		addtodbpanel.add(lblReleaseDate, gbc_lblReleaseDate);
+		addNewRelease.add(lblReleaseDate, gbc_lblReleaseDate);
 		
-		datepicker = new JDatePicker();
-		//datepicker.getDateFormat().
+		newReleaseDate = new JDatePicker();
 		GridBagConstraints gbc_btnNewDatePicker = new GridBagConstraints();
 		gbc_btnNewDatePicker.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnNewDatePicker.insets = new Insets(0, 0, 5, 5);
 		gbc_btnNewDatePicker.gridx = 1;
 		gbc_btnNewDatePicker.gridy = 4;
-		addtodbpanel.add(datepicker, gbc_btnNewDatePicker);
+		addNewRelease.add(newReleaseDate, gbc_btnNewDatePicker);
 		
 		JLabel lblUrl = new JLabel("URL");
 		GridBagConstraints gbc_lblUrl = new GridBagConstraints();
 		gbc_lblUrl.insets = new Insets(0, 0, 5, 5);
 		gbc_lblUrl.gridx = 0;
 		gbc_lblUrl.gridy = 5;
-		addtodbpanel.add(lblUrl, gbc_lblUrl);
+		addNewRelease.add(lblUrl, gbc_lblUrl);
 		
 		urlfield = new JTextField();
 		GridBagConstraints gbc_urlfield = new GridBagConstraints();
@@ -180,10 +238,10 @@ public class guiframe extends JFrame{
 		gbc_urlfield.fill = GridBagConstraints.HORIZONTAL;
 		gbc_urlfield.gridx = 1;
 		gbc_urlfield.gridy = 5;
-		addtodbpanel.add(urlfield, gbc_urlfield);
+		addNewRelease.add(urlfield, gbc_urlfield);
 		urlfield.setColumns(10);
 		
-		JTextPane textPane = new JTextPane();
+		textPane = new JTextPane();
 		GridBagConstraints gbc_textPane = new GridBagConstraints();
 		gbc_textPane.gridwidth = 2;
 		gbc_textPane.gridheight = 5;
@@ -191,7 +249,55 @@ public class guiframe extends JFrame{
 		gbc_textPane.fill = GridBagConstraints.BOTH;
 		gbc_textPane.gridx = 1;
 		gbc_textPane.gridy = 6;
-		addtodbpanel.add(textPane, gbc_textPane);
+		addNewRelease.add(textPane, gbc_textPane);
+		
+		JToggleButton tglbtnSubmit = new JToggleButton("Submit");
+		tglbtnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int result;
+				//make sure the primary keys are included when searching
+				/*if(artistfield.getText().equals("") || releasenamefield.getText().equals("") 
+						|| comboBoxReleaseType.getSelectedIndex() == -1
+						|| datepicker.getFormattedTextField().getText().equals("")) {
+						textPane.setText("A new release must have an artist, name, and type, and date!");
+						return;
+				}
+				java.util.GregorianCalendar selectedDate = (GregorianCalendar) datepicker.getModel().getValue();
+				Calendar calendar = (Calendar)selectedDate;
+				java.sql.Date date = new java.sql.Date(calendar.getTimeInMillis());*/
+				java.util.Date date = java.sql.Date.valueOf("2018-03-21");
+				java.sql.Date sqlStartDate = new java.sql.Date(date.getTime());  
+				System.out.println(sqlStartDate);
+				//result = mydatabase.insertData(mydatabase, artistfield.getText(), releasenamefield.getText(), (String)comboBoxReleaseType.getSelectedItem(),
+				//		date, urlfield.getText());
+				result = mydatabase.insertData(mydatabase, "MONKEY MAJIK", "enigma", "album",
+						sqlStartDate, "");
+				if(result == 1) {
+					clearInputs();
+					textPane.setText("New release added to the database successfully!");
+				}
+				else if(result == 0) {
+					textPane.setText("New release could not be add to the database! It already exists.");
+				}
+			}
+		});
+		GridBagConstraints gbc_tglbtnSubmit = new GridBagConstraints();
+		gbc_tglbtnSubmit.insets = new Insets(0, 0, 5, 5);
+		gbc_tglbtnSubmit.gridx = 1;
+		gbc_tglbtnSubmit.gridy = 0;
+		addNewRelease.add(tglbtnSubmit, gbc_tglbtnSubmit);
+		
+		JButton btnClearAll = new JButton("Clear All");
+		btnClearAll.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				clearInputs();
+			}
+		});
+		GridBagConstraints gbc_btnClearAll = new GridBagConstraints();
+		gbc_btnClearAll.insets = new Insets(0, 0, 5, 5);
+		gbc_btnClearAll.gridx = 0;
+		gbc_btnClearAll.gridy = 0;
+		addNewRelease.add(btnClearAll, gbc_btnClearAll);
 		
 		pack();
 		setLocationRelativeTo(null);
@@ -203,6 +309,14 @@ public class guiframe extends JFrame{
 	}
 	public void setDriver(sqlfunctions mydatabase) {
 		this.mydatabase = mydatabase;
+	}
+	public void clearInputs() {
+		artistfield.setText("");
+		releasenamefield.setText("");
+		comboBoxReleaseType.setSelectedIndex(-1);
+		urlfield.setText("");
+		textPane.setText("");
+		newReleaseDate.getFormattedTextField().setText("");
 	}
 	
 }
--- src/jmusicreleasemanager/sqlfunctions.java
+++ src/jmusicreleasemanager/sqlfunctions.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Vector;
 /**
  * My custom Driver for JDBC
  * @author Jonathan
@@ -33,28 +34,146 @@ public class sqlfunctions {
 		System.out.println("Connected to the database....");
 		
 	}
-	public void insertData(sqlfunctions sqlfunctionsDriver, String ARTIST, String ReleaseName, String ReleaseType, java.sql.Date ReleaseDate, 
-			String URL, String Edition, String Label, String Catalog, String musicbrainz, String discogs) {
-		String query = " insert into releases (ARTIST, ReleaseName, ReleaseType, ReleaseDate, URL, Edition, Label, Catalog, musicbrainz, discogs)"
-				+" values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	/**
+	 * Retrieves and artist's id number from the db given the artist's name
+	 * An existing artist will have a non zero and non negative number
+	 * @param sqlfunctionsDriver
+	 * @param ARTIST
+	 * @return
+	 */
+	public int retrieveArtistId(sqlfunctions sqlfunctionsDriver, String ARTIST) {
+		String query = "SELECT id FROM artist WHERE name=?;";
 		PreparedStatement preparedStmt;
+		ResultSet resultSet;
 		try {
 			preparedStmt = sqlfunctionsDriver.myConn.prepareStatement(query);
 			preparedStmt.setString (1, ARTIST);
-			preparedStmt.setString (2, ReleaseName);
-			preparedStmt.setString (3, ReleaseType);
-			preparedStmt.setDate(4, ReleaseDate);
-	      	preparedStmt.setString (5, URL);
-	      	preparedStmt.setString (6, Edition);
-	      	preparedStmt.setString (7, Label);
-	      	preparedStmt.setString (8, Catalog);
-	      	preparedStmt.setString (9, musicbrainz);
-	      	preparedStmt.setString (10, discogs);
-	      	preparedStmt.execute();
-		} catch (SQLException e) {
-			System.out.println("Error. Unable to insert into the database.");
+			resultSet = preparedStmt.executeQuery();
+			if(resultSet.next()) {
+				return resultSet.getInt("id");
+			}
+		}catch (SQLException e) {
+			System.out.println("Error. Unable to retrieve an artist from the database.");
 			e.printStackTrace();
 		}
+		return 0;
+		
+	}
+	/**
+	 * Inserts a new row in the release table
+	 * @param sqlfunctionsDriver
+	 * @param ARTIST
+	 * @param ReleaseName
+	 * @param ReleaseType
+	 * @param ReleaseDate
+	 * @param URL
+	 * @return
+	 */
+	public int insertData(sqlfunctions sqlfunctionsDriver, String ARTIST, String ReleaseName, String ReleaseType, java.sql.Date ReleaseDate, 
+			String URL) {
+		//get the artist id
+		int artistId = retrieveArtistId(sqlfunctionsDriver, ARTIST);
+		if(artistId == 0) {//when no artist exists need to create a new artist entry
+			addArtist(sqlfunctionsDriver, ARTIST, "");//Kanji is ignored here
+			//get the artist id
+			artistId = retrieveArtistId(sqlfunctionsDriver, ARTIST);
+		}
+		boolean exists = check_dup(sqlfunctionsDriver, artistId, ReleaseName, ReleaseType, ReleaseDate);
+			if(!exists) {
+				String query = "INSERT INTO jmusicrelease.release (name, date, type, artistId, url)"
+						+" values (?, ?, ?, ?, ?)";
+				
+				PreparedStatement preparedStmt;
+				try {
+					preparedStmt = sqlfunctionsDriver.myConn.prepareStatement(query);
+					preparedStmt.setString (1, ReleaseName);
+					preparedStmt.setDate(2, ReleaseDate);
+					preparedStmt.setString (3, ReleaseType);
+					preparedStmt.setInt(4, artistId);
+			      	preparedStmt.setString (5, URL);
+			      	preparedStmt.execute();
+				} catch (SQLException e) {
+					System.out.println("Error. Unable to insert a new release into the database.");
+					e.printStackTrace();
+				}
+			}
+			else {
+				return 0; //record already exists
+			}
+	return 1; //No need to cancel any insertions
+	}
+	
+	/**
+	 * Adds a new artist to the artist table
+	 * @param artist
+	 */
+	public void addArtist(sqlfunctions sqlfunctionsDriver, String name, String kanji) {
+		String query = " INSERT INTO jmusicrelease.artist (name, kanji)"
+				+" values (?, ?)";
+		PreparedStatement preparedStmt;
+		try {
+			preparedStmt = sqlfunctionsDriver.myConn.prepareStatement(query);
+			preparedStmt.setString (1, name);
+			preparedStmt.setString(2, kanji);
+	      	preparedStmt.execute();
+		} catch (SQLException e) {
+			System.out.println("Error. Unable to insert a new artist into the database.");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * Checks if a row exists according to given conditions
+	 * @param sqlfunctionsDriver
+	 * @param artistId
+	 * @param ReleaseName
+	 * @param ReleaseType
+	 * @param ReleaseDate
+	 * @return
+	 */
+	public boolean check_dup(sqlfunctions sqlfunctionsDriver, int artistId, String ReleaseName, String ReleaseType, java.sql.Date ReleaseDate) {
+		String query = "SELECT * FROM jmusicrelease.release WHERE artistId = ? And name = ? And type = ? And date = ?";
+		PreparedStatement preparedStmt;
+		boolean check = false;
+		try {
+			preparedStmt = sqlfunctionsDriver.myConn.prepareStatement(query);
+			preparedStmt.setInt (1, artistId);
+			preparedStmt.setString (2, ReleaseName);
+			preparedStmt.setString (3, ReleaseType);
+			preparedStmt.setDate (4, ReleaseDate);
+			ResultSet resultSet = preparedStmt.executeQuery();
+			if(resultSet.next()){
+	            check =  true;
+	        }else{
+	            check = false;
+	        }
+		} catch (SQLException e) {
+			System.out.println("Error. Unable to check for duplicates in the database.");
+			e.printStackTrace();
+		}
+		return check;
+		
+	}
+	public ResultSet retrieveReleases(sqlfunctions sqlfunctionsDriver, java.sql.Date afterD, java.sql.Date beforeD){
+		PreparedStatement preparedStmt;
+		ResultSet rs = null;
+		String query = "SELECT release.name, artist.name AS artist, release.date, release.type\r\n" + 
+				"FROM jmusicrelease.release, jmusicrelease.artist\r\n" + 
+				"WHERE release.artistId=artist.id AND release.date >= '"+afterD.toString()+"'";
+		if(beforeD!=null)
+		{
+			query+="AND release.date <= '"+beforeD.toString()+"'";
+		}
+				 
+		query+="\nORDER BY release.date;";
+		try {
+			preparedStmt = sqlfunctionsDriver.myConn.prepareStatement(query);
+			rs = preparedStmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
 	}
 	public void updateData(sqlfunctions sqlfunctionsDriver, String ARTIST, String ReleaseName, String ReleaseType, java.sql.Date ReleaseDate, 
 			String URL, String Edition, String Label, String Catalog, String musicbrainz, String discogs) {
@@ -83,29 +202,5 @@ public class sqlfunctions {
 			System.out.println("Error. Unable to update the database.");
 			e.printStackTrace();
 		}
-	}
-	public boolean check_dup(sqlfunctions sqlfunctionsDriver, String ARTIST, String ReleaseName, String ReleaseType, String Edition, java.sql.Date ReleaseDate) {
-		String query = "SELECT * FROM releases WHERE ARTIST= ? And ReleaseName= ? And ReleaseType= ? And Edition= ? And ReleaseDate= ?";
-		PreparedStatement preparedStmt;
-		boolean check = false;
-		try {
-			preparedStmt = sqlfunctionsDriver.myConn.prepareStatement(query);
-			preparedStmt.setString (1, ARTIST);
-			preparedStmt.setString (2, ReleaseName);
-			preparedStmt.setString (3, ReleaseType);
-			preparedStmt.setString (4, Edition);
-			preparedStmt.setDate (5, ReleaseDate);
-			ResultSet resultSet = preparedStmt.executeQuery(query);
-			if(resultSet.next()){
-	            check =  true;
-	        }else{
-	            check = false;
-	        }
-		} catch (SQLException e) {
-			System.out.println("Error. Unable to check for duplicates in the database.");
-			e.printStackTrace();
-		}
-		return check;
-		
 	}
 }
